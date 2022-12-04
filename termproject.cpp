461d98070b1fd70d7316b5d40186b635f7e9a325
V2LLAIN <104286511+V2LLAIN@users.noreply.github.com>
2022-12-04 18:14:35 +0900

    Update termproject.cpp

--- termproject.cpp
+++ termproject.cpp
@@ -255,305 +255,295 @@ bool Delete::delete_byte(int line_position, int idx_position, int del_byte) {
     int i = 1;
 
     for (; getline(ss, stringBuffer); i++) {  //getline(istream& is, string str, char delim);
-        vector<char> c(76);
-        vector<string>sss;
-        sss.push_back(stringBuffer);
-
-        for (auto x : sss) {
-            for (int i = 0; i < 76; i++) {
-                c[i] = x[i];
-            }
-        }
-
-        if (i + 1 == idx_position) {
-            c.erase(c.begin() + idx_position, c.begin() + idx_position + del_byte + 1);
-        }
-        else {
-            for (auto x : c)
-                str += x;
-        }
-        /*
-        char c[76];
-        for (int j = 0; j < line_position; j++) {
-            for(int i = 0; i < 76; i++){
-                c[j] = stringBuffer[j];
-            }
-
-        }*/
-    }
-    v[line_position - 1] = str;
-    return true;
-}
-class Change : public Strategy_File {
-private:
-    bool change(string a, string b);
-public:
-    bool do_action(string input_s) {
-        bool flag = true;
-        string change_str[2];
-        string str_temp;
-        if (input_s[1] != '(' || input_s[input_s.length() - 1] != ')') {
-            //명령어 다음 입력값과 마지막 입력값은 괄호여야한다.
-            cout << "괄호를 잘못입력하셨습니다. 다시 입력해주세요" << endl;
-            return false;
-        }
-        //괄호 안에 공백을 입력하면 입력오류
-        for (int i = 2; i < input_s.length() - 1; i++) {
-            if (input_s[i] == ' ') {
-                cout << "괄호안에 공백을 입력하셨습니다. 다시입력해주세요" << endl;
-                flag = false;
-                return false;
-            }
-        }
-        if (flag == false) { return false; }
-        for (int i = 2; i < input_s.length(); i++) {
-            if (input_s[i] == ',') {
-                change_str[0] = str_temp;
-                str_temp = "";
-            }
-            else if (input_s[i] == ')') {
-                change_str[1] = str_temp;
-            }
-            else
-                str_temp += input_s[i];
-
-        }
-        if (change_str[0].size() == 0 || change_str[1].size() == 0) { //하나도 인자가 입력이 제대로 되지 않았으면 ""일것이다.
-            cout << "형식에 맞는 입력값이 아닙니다. 다시 입력해 주세요." << endl;
-            return false;
-        }
-        if (!change(change_str[0], change_str[1])) {
-            return false;
-        }
-        return true;
-    }
-};
-
-bool Change::change(string prev_str, string modify_str)
-{
-    int count = 0;
-    for (int i = 0; i < v.size(); i++) {
         string str;
         string stringBuffer;
-        istringstream ss(v[i]);
-        while (getline(ss, stringBuffer, ' ')) {
-            if (stringBuffer == prev_str) {
-                str += modify_str + " ";
-                count++;
+        istringstream ss(v[line_position - 1]);
+        int i = 1;
+        for (; getline(ss, stringBuffer, ' '); i++) {
+            //int t = stringBuffer.size();
+            if (i == idx_position)
                 continue;
-            }
             else
                 str += stringBuffer + " ";
         }
-        v[i] = str;
-    }
-    if (count == 0) {
-        //바꾸려고 하였던 문자가 존재하지 않은것
-        cout << "변경할 문자가 존재하지 않습니다. 다시입력해주세요." << endl;
-        return false;
-    }
-    return true;
-}
-
-class Search : public Strategy_File {
-private:
-    bool search(string s);
-public:
-    bool do_action(string input_s) {
-        bool flag = true;
-        string str_temp;
-        if (input_s[1] != '(' || input_s[input_s.length() - 1] != ')') {
-            //명령어 다음 입력값과 마지막 입력값은 괄호여야한다.
-            cout << "괄호를 잘못입력하셨습니다. 다시 입력해주세요" << endl;
+        if (i <= idx_position) { //현재 라인에 원하는 단어가 존재하지 않는 경우
+            cout << line_position << "번째 라인에는 " << idx_position << "번째 단어가 존재하지 않습니다. 다시 입력해주세요." << endl;
             return false;
         }
-        //괄호 안에 공백을 입력하면 입력오류
-        for (int i = 2; i < input_s.length() - 1; i++) {
-            if (input_s[i] == ' ') {
-                cout << "괄호안에 공백을 입력하셨습니다. 다시입력해주세요" << endl;
-                flag = false;
+        v[line_position - 1] = str;
+        return true;
+
+    }
+}
+    class Change : public Strategy_File {
+    private:
+        bool change(string a, string b);
+    public:
+        bool do_action(string input_s) {
+            bool flag = true;
+            string change_str[2];
+            string str_temp;
+            if (input_s[1] != '(' || input_s[input_s.length() - 1] != ')') {
+                //명령어 다음 입력값과 마지막 입력값은 괄호여야한다.
+                cout << "괄호를 잘못입력하셨습니다. 다시 입력해주세요" << endl;
                 return false;
             }
-        }
-        if (flag == false)
-            return false;
-        str_temp = input_s.substr(2, input_s.length() - 3);
-        if (!search(str_temp + " ")) {
-            cout << "찾는 문자가 존재하지 않습니다. 다시입력해주세요" << endl;
-            return false;
-        }
-        //찾는 문자가 존재하면
-        return true;
-    }
-};
+            //괄호 안에 공백을 입력하면 입력오류
+            for (int i = 2; i < input_s.length() - 1; i++) {
+                if (input_s[i] == ' ') {
+                    cout << "괄호안에 공백을 입력하셨습니다. 다시입력해주세요" << endl;
+                    flag = false;
+                    return false;
+                }
+            }
+            if (flag == false) { return false; }
+            for (int i = 2; i < input_s.length(); i++) {
+                if (input_s[i] == ',') {
+                    change_str[0] = str_temp;
+                    str_temp = "";
+                }
+                else if (input_s[i] == ')') {
+                    change_str[1] = str_temp;
+                }
+                else
+                    str_temp += input_s[i];
 
-bool Search::search(string search_word) {
-    string str;
-    for (int i = 0; i < v.size(); i++) {
-        //반환값이 string::npos이면 찾는 문자가 없는것이다.
-        str = v[i];
-        if (str.find(search_word) != string::npos) {
-            //문자를 찾은것
-            LINE = i + 1;
+            }
+            if (change_str[0].size() == 0 || change_str[1].size() == 0) { //하나도 인자가 입력이 제대로 되지 않았으면 ""일것이다.
+                cout << "형식에 맞는 입력값이 아닙니다. 다시 입력해 주세요." << endl;
+                return false;
+            }
+            if (!change(change_str[0], change_str[1])) {
+                return false;
+            }
             return true;
         }
-    }
-    return false;
-}
+    };
 
-class SaveEnd : public Strategy_File {
-private:
-    void saveFile();
-public:
-    bool do_action(string input_s) {
-        saveFile();
-        cout << "변경내용을 파일에 저장하였습니다. 프로그램을 종료합니다." << endl;
+    bool Change::change(string prev_str, string modify_str)  {
+        int count = 0;
+        for (int i = 0; i < v.size(); i++) {
+            string str;
+            string stringBuffer;
+            istringstream ss(v[i]);
+            while (getline(ss, stringBuffer, ' ')) {
+                if (stringBuffer == prev_str) {
+                    str += modify_str + " ";
+                    count++;
+                    continue;
+                }
+                else
+                    str += stringBuffer + " ";
+            }
+            v[i] = str;
+        }
+        if (count == 0) {
+            //바꾸려고 하였던 문자가 존재하지 않은것
+            cout << "변경할 문자가 존재하지 않습니다. 다시입력해주세요." << endl;
+            return false;
+        }
+        return true;
+    }
+
+    class Search : public Strategy_File {
+    private:
+        bool search(string s);
+    public:
+        bool do_action(string input_s) {
+            bool flag = true;
+            string str_temp;
+            if (input_s[1] != '(' || input_s[input_s.length() - 1] != ')') {
+                //명령어 다음 입력값과 마지막 입력값은 괄호여야한다.
+                cout << "괄호를 잘못입력하셨습니다. 다시 입력해주세요" << endl;
+                return false;
+            }
+            //괄호 안에 공백을 입력하면 입력오류
+            for (int i = 2; i < input_s.length() - 1; i++) {
+                if (input_s[i] == ' ') {
+                    cout << "괄호안에 공백을 입력하셨습니다. 다시입력해주세요" << endl;
+                    flag = false;
+                    return false;
+                }
+            }
+            if (flag == false)
+                return false;
+            str_temp = input_s.substr(2, input_s.length() - 3);
+            if (!search(str_temp + " ")) {
+                cout << "찾는 문자가 존재하지 않습니다. 다시입력해주세요" << endl;
+                return false;
+            }
+            //찾는 문자가 존재하면
+            return true;
+        }
+    };
+
+    bool Search::search(string search_word) {
+        string str;
+        for (int i = 0; i < v.size(); i++) {
+            //반환값이 string::npos이면 찾는 문자가 없는것이다.
+            str = v[i];
+            if (str.find(search_word) != string::npos) {
+                //문자를 찾은것
+                LINE = i + 1;
+                return true;
+            }
+        }
         return false;
     }
-};
-void SaveEnd::saveFile() {
-    ofstream out("test.txt");
-    for (int i = 0; i < v.size(); i++)
-        out << v[i];
-    out.close();
-}
 
-int error_flag = false;
-class FileContext {
-public:
-    FileContext(Strategy_File* sf) {
-        StrategyFile = sf;
-        setVector();
-        initPage();
+    class SaveEnd : public Strategy_File {
+    private:
+        void saveFile();
+    public:
+        bool do_action(string input_s) {
+            saveFile();
+            cout << "변경내용을 파일에 저장하였습니다. 프로그램을 종료합니다." << endl;
+            return false;
+        }
+    };
+    void SaveEnd::saveFile() {
+        ofstream out("test.txt");
+        for (int i = 0; i < v.size(); i++)
+            out << v[i];
+        out.close();
     }
-    void setVector() {
-        ifstream in("test.txt");
+
+    int error_flag = false;
+    class FileContext {
+    public:
+        FileContext(Strategy_File* sf) {
+            StrategyFile = sf;
+            setVector();
+            initPage();
+        }
+        void setVector() {
+            ifstream in("test.txt");
+            string s, str;
+            int size = 0;
+            int temp = 0;
+            while (!in.eof()) {
+                in >> s;
+                size = (int)s.size();
+                if (size + temp <= 75) {
+                    str += s + " ";
+                    temp += size + 1;
+                }
+                else {
+                    v.push_back(str);
+                    str = s + " ";
+                    temp = size + 1;
+                }
+            }
+            if (in.eof()) {
+                v.push_back(str);
+            }
+            in.close();
+
+            vec_size = v.size();
+        }
+        void initPage();
+        void modifyVector();
+        void thisPage();
+        bool action() {
+            string input;
+            if (print_flag) {
+                cout << "----------------------------------------------------------------------------------" << endl;
+                cout << "n:다음페이지, p:이전페이지, i:삽입, d:삭제, c:변경, s:찾기, t:저장후 종료" << endl;
+            }
+            print_flag = true;
+            getline(cin, input); //한줄 입력받기
+
+            this->setStratage(input); //action 변경
+            if (error_flag) {   //잘못된 명령어일경우 리턴
+                error_flag = false;
+                return false;
+            }
+            bool result = StrategyFile->do_action(input);
+            if ((char)input[0] == 't') {
+                return result;
+            }
+            if (result) {
+                modifyVector();
+                thisPage();
+            }
+            return true;
+        }
+        void setStratage(string input) {
+            char keyword = (char)input[0];
+            switch (keyword) {
+                case 'n':
+                    StrategyFile = new NextPage;
+                    break;
+                case 'p':
+                    StrategyFile = new PrevPage;
+                    break;
+                case 'i':
+                    StrategyFile = new Insert;
+                    break;
+                case 'd':
+                    StrategyFile = new Delete;
+                    break;
+                case 'c':
+                    StrategyFile = new Change;
+                    break;
+                case 's':
+                    StrategyFile = new Search;
+                    break;
+                case 't':
+                    StrategyFile = new SaveEnd;
+                    break;
+
+                default:
+                    if (input == "") {
+                        print_flag = false;
+                        break;
+                    }
+                    error_flag = true;
+                    cout << "명령어를 잘못 입력하셨습니다. 다시 입력해주세요." << endl;
+                    return;
+            }
+        }
+    protected:
+        Strategy_File* StrategyFile;
+        bool print_flag = true;
+    };
+
+    void FileContext::modifyVector() {
         string s, str;
+        //한 문자열로 합쳐준다.
+        for (int i = 0; i < v.size(); i++)
+            s += v[i];
+        v.clear();
         int size = 0;
         int temp = 0;
-        while (!in.eof()) {
-            in >> s;
-            size = (int)s.size();
+        int i = 0;
+        string stringBuffer;
+        istringstream ss(s);
+        while (getline(ss, stringBuffer, ' ')) {
+            size = (int)stringBuffer.size();
             if (size + temp <= 75) {
-                str += s + " ";
+                str += stringBuffer + " ";
                 temp += size + 1;
             }
             else {
                 v.push_back(str);
-                str = s + " ";
+                str = stringBuffer + " ";
                 temp = size + 1;
             }
         }
-        if (in.eof()) {
-            v.push_back(str);
-        }
-        in.close();
-
+        v.push_back(str);
         vec_size = v.size();
     }
-    void initPage();
-    void modifyVector();
-    void thisPage();
-    bool action() {
-        string input;
-        if (print_flag) {
-            cout << "----------------------------------------------------------------------------------" << endl;
-            cout << "n:다음페이지, p:이전페이지, i:삽입, d:삭제, c:변경, s:찾기, t:저장후 종료" << endl;
-        }
-        print_flag = true;
-        getline(cin, input); //한줄 입력받기
-
-        this->setStratage(input); //action 변경
-        if (error_flag) {   //잘못된 명령어일경우 리턴
-            error_flag = false;
-            return false;
-        }
-        bool result = StrategyFile->do_action(input);
-        if ((char)input[0] == 't') {
-            return result;
-        }
-        if (result) {
-            modifyVector();
-            thisPage();
-        }
-        return true;
+    void FileContext::initPage() {
+        LINE = 1;
+        thisPage();
     }
-    void setStratage(string input) {
-        char keyword = (char)input[0];
-        switch (keyword) {
-        case 'n':
-            StrategyFile = new NextPage;
-            break;
-        case 'p':
-            StrategyFile = new PrevPage;
-            break;
-        case 'i':
-            StrategyFile = new Insert;
-            break;
-        case 'd':
-            StrategyFile = new Delete;
-            break;
-        case 'c':
-            StrategyFile = new Change;
-            break;
-        case 's':
-            StrategyFile = new Search;
-            break;
-        case 't':
-            StrategyFile = new SaveEnd;
-            break;
-
-        default:
-            if (input == "") {
-                print_flag = false;
-                break;
-            }
-            error_flag = true;
-            cout << "명령어를 잘못 입력하셨습니다. 다시 입력해주세요." << endl;
-            return;
-        }
+    void FileContext::thisPage() {
+        for (i = LINE; i < LINE + 20 && v.size(); i++)
+            cout << i << " | " << v[i - 1] << endl;
     }
-protected:
-    Strategy_File* StrategyFile;
-    bool print_flag = true;
-};
 
-void FileContext::modifyVector() {
-    string s, str;
-    //한 문자열로 합쳐준다.
-    for (int i = 0; i < v.size(); i++)
-        s += v[i];
-    v.clear();
-    int size = 0;
-    int temp = 0;
-    int i = 0;
-    string stringBuffer;
-    istringstream ss(s);
-    while (getline(ss, stringBuffer, ' ')) {
-        size = (int)stringBuffer.size();
-        if (size + temp <= 75) {
-            str += stringBuffer + " ";
-            temp += size + 1;
-        }
-        else {
-            v.push_back(str);
-            str = stringBuffer + " ";
-            temp = size + 1;
-        }
+    int main() {
+        FileContext* f1 = new FileContext(new NextPage);
+        while (f1->action());
     }
-    v.push_back(str);
-    vec_size = v.size();
-}
-void FileContext::initPage() {
-    LINE = 1;
-    thisPage();
-}
-void FileContext::thisPage() {
-    for (i = LINE; i < LINE + 20 && v.size(); i++)
-        cout << i << " | " << v[i - 1] << endl;
-}
-
-int main() {
-    FileContext* f1 = new FileContext(new NextPage);
-    while (f1->action());
-}
